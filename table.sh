b4227284aca3574d49af2544f36193f565e4e926
Sonal <sonal.nagesh@gmail.com>
2026-07-23 15:15:52 +0530

    Update table.sh

--- table.sh
+++ table.sh
@@ -1,7 +1,5 @@
 qiime feature-table summarize \
-  --i-table table.qza \
-  --o-visualization table.qzv \
-  --m-sample-metadata-file sample-metadata.tsv
-qiime feature-table tabulate-seqs \
-  --i-data rep-seqs.qza \
-  --o-visualization rep-seqs.qzv
+    --i-table table.qza \
+    --o-feature-frequencies feature-frequencies.qza \
+    --o-sample-frequencies sample-frequencies.qza \
+    --o-summary visual-summary.qzv
