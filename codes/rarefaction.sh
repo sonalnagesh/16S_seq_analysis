1ab8dd0197730500006383222ef82c8ca1ed0d06
Sonal <sonal.nagesh@gmail.com>
2026-07-23 16:43:02 +0530

    Update rarefaction.sh

--- codes/rarefaction.sh
+++ codes/rarefaction.sh
@@ -2,5 +2,5 @@ qiime diversity alpha-rarefaction \
   --i-table table.qza \
   --i-phylogeny rooted-tree.qza \
   --p-max-depth 250000 \
-  --m-metadata-file manifest2.tsv \
+  --m-metadata-file manifest.tsv \
   --o-visualization alpha-rarefaction.qzv
