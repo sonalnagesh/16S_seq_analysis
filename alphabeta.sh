55374c8fb3d2c155f3128a174504b9f121d6e397
Sonal <sonal.nagesh@gmail.com>
2026-07-23 16:28:22 +0530

    Create alphabeta.sh

---
+++ alphabeta.sh
@@ -0,0 +1,6 @@
+qiime diversity core-metrics-phylogenetic \
+  --i-phylogeny rooted-tree.qza \
+  --i-table table.qza \
+  --p-sampling-depth 1103 \
+  --m-metadata-file sample-metadata.tsv \
+  --output-dir core-metrics-results
