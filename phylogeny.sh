5f20eee65400ab020ca32f362cdc7138aefff627
Sonal <sonal.nagesh@gmail.com>
2026-07-23 16:14:37 +0530

    Update phylogeny.sh

--- phylogeny.sh
+++ phylogeny.sh
@@ -1,4 +1,4 @@
-qiime phylogeny align-to-tree-mafft-fasttree \
+ qiime phylogeny align-to-tree-mafft-fasttree \
   --i-sequences rep-seqs.qza \
   --o-alignment aligned-rep-seqs.qza \
   --o-masked-alignment masked-aligned-rep-seqs.qza \
