8a8650fc57017f5bbdecbd81dde8ee4e5fb1eac5
Sonal <sonal.nagesh@gmail.com>
2026-07-23 14:56:02 +0530

    Create phylogeny.sh

---
+++ phylogeny.sh
@@ -0,0 +1,6 @@
+qiime phylogeny align-to-tree-mafft-fasttree \
+  --i-sequences rep-seqs.qza \
+  --o-alignment aligned-rep-seqs.qza \
+  --o-masked-alignment masked-aligned-rep-seqs.qza \
+  --o-tree unrooted-tree.qza \
+  --o-rooted-tree rooted-tree.qza
