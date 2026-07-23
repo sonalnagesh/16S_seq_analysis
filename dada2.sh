0c1565fa05a371cadf829fa4072511de0b9589ab
Sonal <sonal.nagesh@gmail.com>
2026-07-23 15:12:06 +0530

    Update dada2.sh

--- dada2.sh
+++ dada2.sh
@@ -1,9 +1,10 @@
-qiime dada2 denoise-paired \
-  --i-demultiplexed-seqs paired-end-demux.qza \
-  --p-trim-left-f 30 \
-  --p-trim-left-r 30 \
-  --p-trunc-len-f 190 \
-  --p-trunc-len-r 140 \
-  --o-table table.qza \
-  --o-representative-sequences rep-seqs.qza \
-  --o-denoising-stats denoising-stats.qza
+ qiime dada2 denoise-paired \
+   --i-demultiplexed-seqs paired-end-demux.qza \
+   --p-trim-left-f 30 \
+   --p-trim-left-r 30 \
+   --p-trunc-len-f 190 \
+   --p-trunc-len-r 140 \
+   --o-table table.qza \
+   --o-representative-sequences rep-seqs.qza \
+   --o-denoising-stats denoising-stats.qza \
+   --o-base-transition-stats base-transition-stats.qza
